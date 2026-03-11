a84e0ff4b00b90e606538a7b28e9c2af9f242167
SnickersOda <hyunixempires@gmail.com>
2026-03-11 20:16:39 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 const express = require('express');
 const { ExpressPeerServer } = require('peer');
 const path = require('path');
+const http = require('http');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Отдаём HTML
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'calls.html'));
 });
 
-const server = require('http').createServer(app);
+const server = http.createServer(app);
 
-// Свой PeerJS сервер на том же домене
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-  path: '/'
-});
-
+const peerServer = ExpressPeerServer(server, { debug: true });
 app.use('/peerjs', peerServer);
 
-server.listen(PORT, () => console.log(`Running on port ${PORT}`));
+server.listen(PORT, () => console.log('Running on port ' + PORT));
